52f735f2e53f69d5c9eee1073d1b19157d01563b
perrycarbonell <perry.carbonell@gmail.com>
2013-08-17 06:23:07 +1000

    Add Pins Association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
 
   # GET /pins
   def index
@@ -12,16 +13,17 @@ class PinsController < ApplicationController
 
   # GET /pins/new
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.new
   end
 
   # GET /pins/1/edit
   def edit
+    @pin = current_user.pins.find(params[:id])
   end
 
   # POST /pins
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.new(pin_params)
 
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
@@ -32,15 +34,17 @@ class PinsController < ApplicationController
 
   # PATCH/PUT /pins/1
   def update
-    if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.'
-    else
-      render action: 'edit'
-    end
+    @pin = current_user.pins.find(params[:id])
+      if @pin.update(pin_params)
+        redirect_to @pin, notice: 'Pin was successfully updated.'
+      else
+        render action: 'edit'
+      end
   end
 
   # DELETE /pins/1
   def destroy
+    @pin = current_user.pins.find(params[:id])
     @pin.destroy
     redirect_to pins_url, notice: 'Pin was successfully destroyed.'
   end
