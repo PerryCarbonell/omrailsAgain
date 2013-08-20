2974882865725dde43cdcf9a51d3ceb57cf08cd6
perrycarbonell <perry.carbonell@gmail.com>
2013-08-21 05:48:11 +1000

    Add jQuery Masonry

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -1,2 +1,7 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file http://jashkenas.github.io/coffee-script/
+
+jQuery ->
+	$('#pins').imagesLoaded ->
+		$('#pins').masonry itemSelector:".box"
